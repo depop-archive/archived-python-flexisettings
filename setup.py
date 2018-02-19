61615099c62f3a9e0ccf90bc7ef88ed105d70a12
Anentropic <ego@anentropic.com>
2018-02-19 16:07:50 +0000

    auto push git tag from makefile

--- setup.py
+++ setup.py
@@ -8,13 +8,19 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Get content from __about__.py
+about = {}
+with open(path.join(here, 'flexisettings', '__about__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+
 setup(
     name='flexisettings',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version=about['__version__'],
 
     description='Overridable config for shared libraries.',
     long_description=long_description,
